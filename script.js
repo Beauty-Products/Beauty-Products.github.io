04b7eaf236f9c847ff7765823819a68732c763c9
Beauty-products <beatyfulproducts@gmail.com>
2025-01-22 20:30:23 -0500

    commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function btnnosotros() {
     const nosotrosDiv = document.querySelector('.nosotros');
     if (nosotrosDiv) {
-      const currentLeft = nosotrosDiv.style.left;
-      if (currentLeft === '78%') {
-        nosotrosDiv.style.left = '110%';
+      const top = nosotrosDiv.style.top;
+      if (top === '-90%') {
+        nosotrosDiv.style.top = '1%';
       } else {
-        nosotrosDiv.style.left = '78%';
+        nosotrosDiv.style.top = '-90%';
       }
     }
   }
